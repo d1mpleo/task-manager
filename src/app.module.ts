2ca6f8b005159460e39f8a16935f96b2e18d5cbf
tehNarik <2016mishasimonenko@gmail.com>
2026-04-17 15:49:54 +0200

    db changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,16 @@ import { UserModule } from './user/user.module';
   imports: [TasksModule,
     TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'pass1234',
-  database: 'task-management',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   autoLoadEntities: true,
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 }),
     AuthModule,
     UserModule
