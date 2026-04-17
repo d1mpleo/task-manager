e2c80f39b7be32d3bce778bedcaaa62531bd17f8
tehNarik <2016mishasimonenko@gmail.com>
2026-04-17 16:17:50 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TasksModule,
+  imports: [
+    TasksModule,
     TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  autoLoadEntities: true,
-  synchronize: true,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-}),
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
     AuthModule,
-    UserModule
+    UserModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
