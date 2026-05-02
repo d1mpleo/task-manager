c9c11ecfdbd734d9db539cc1b43d352e92da20ef
tehNarik <2016mishasimonenko@gmail.com>
2026-05-02 15:02:23 +0200

    Clean main.ts and fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,25 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TasksModule,
+    ConfigModule.forRoot({
+    isGlobal: true,
+  }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      ssl: { rejectUnauthorized: false },
-    }),
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+}),
     AuthModule,
     UserModule,
   ],
